e1de423d243e03536aecfa37593a57ddd27420c5
3 <168254582+220814@users.noreply.github.com>
2026-02-10 21:54:36 +0700

    Update AutoPotModule.java

--- src/main/java/net/shoreline/client/impl/module/combat/AutoPotModule.java
+++ src/main/java/net/shoreline/client/impl/module/combat/AutoPotModule.java
@@ -1,1 +1,15 @@
+package net.shoreline.client.impl.module.combat;
+
+import net.shoreline.client.api.module.ModuleCategory;
+import net.shoreline.client.api.module.ToggleModule;
+import net.shoreline.client.impl.event.entity.player.SprintResetEvent;
+import net.shoreline.eventbus.annotation.EventListener;
+
+public class AutoPotModule extends ToggleModule
+{
+    public AutoPotModule()
+  
+    system.out print("hello example')
+    }
+}
 
