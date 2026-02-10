72bd72feec15e221063dbec3ad8370c7530f41e9
3 <168254582+220814@users.noreply.github.com>
2026-02-10 22:01:18 +0700

    Update AutoPotModule.java

--- src/main/java/net/shoreline/client/impl/module/combat/AutoPotModule.java
+++ src/main/java/net/shoreline/client/impl/module/combat/AutoPotModule.java
@@ -5,11 +5,13 @@ import net.shoreline.client.api.module.ToggleModule;
 import net.shoreline.client.impl.event.entity.player.SprintResetEvent;
 import net.shoreline.eventbus.annotation.EventListener;
 
-public class AutoPotModule extends ToggleModule
-{
-    public AutoPotModule()
-  
-    system.out print("hello example')
+public class AutoPotModule extends ToggleModule {
+
+    public AutoPotModule() {
+        // lol
+        super("AutoPot", "Automatically throws health potions", ModuleCategory.COMBAT);
+        
+        // pls print
+        System.out.println("hello example");
     }
 }
-
